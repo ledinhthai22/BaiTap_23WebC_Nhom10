90452d03bc8587f442d7fb93cb15f6c52a1cd3f5
Phamkhactuyen <phamtuyen27042005@gmail.com>
2025-10-24 13:46:57 +0700

    ViewComponent Index

---
+++ Views/Shared/Components/RecentlyViewed/RecentlyViewed.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BaiTap_23WebC_Nhom10.Models;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using BaiTap_23WebC_Nhom10.Areas.Admin.Controllers;
+namespace BaiTap_23WebC_Nhom10.Views.Shared.Components.RecentlyViewed
+{
+    public class RecentlyViewed : ViewComponent
+    {
+        private readonly HttpClient _httpClient;
+        public RecentlyViewed(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:7021/");
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<Product> products = new List<Product>();
+
+            try
+            {
+                products = await _httpClient.GetFromJsonAsync<List<Product>>("api/products") ?? new List<Product>();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"Lỗi View Component RecentlyViewed: {ex.Message}");
+            }
+
+            return View(products);
+        }
+    }
+}
